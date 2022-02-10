1fc88a16bdfb778d0b04e27e4bd6c76ab175972c
EF <efraisma.ef7@gmail.com>
2022-02-09 21:36:11 -0600

    Escoger color desde la función get_map()

--- scripts/ecobici/map.py
+++ scripts/ecobici/map.py
@@ -53,7 +53,7 @@ class EcoBiciMap:
     def transform(self, station_cols: list=['id','zipCode','location.lat','location.lon'], id_col: str='id', status_col: str='status', bikes_col: str='availability.bikes', slots_col: str='availability.slots') -> None:
         self.df = self.st[station_cols].merge(self.av, on=id_col)
         self.df = self.df[self.df[status_col]=='OPN'].copy()
-        self.df['disponible'] = self.df[bikes_col] / (self.df[slots_col] + self.df[bikes_col])
+        self.df['disponible'] = self.df[slots_col] / (self.df[slots_col] + self.df[bikes_col])
         self.df['ocupado'] = 1 - self.df['disponible']
 
 
@@ -65,7 +65,7 @@ class EcoBiciMap:
         ax.legend(handles=legend_elements, loc="lower right", prop={"size": 6}, ncol=len(values))
 
 
-    def plot_map(self, lat_col: str='location.lat', lon_col: str='location.lon', padding: float=0.007) -> None:
+    def plot_map(self, lat_col: str='location.lat', lon_col: str='location.lon', padding: float=0.007, points_palette: str='mako', **kwargs) -> None:
         fig = Figure(figsize=(5, 4), dpi=200, frameon=False)
         ax = Axes(fig, [0.0, 0.0, 1.0, 1.0])
         fig.add_axes(ax)
@@ -73,20 +73,20 @@ class EcoBiciMap:
         ax.set_ylim((self.df[lat_col].min() - padding, self.df[lat_col].max() + padding))
         ax.set_xlim((self.df[lon_col].min() - padding, self.df[lon_col].max() + padding))
 
-        self.gdf.plot(ax=ax, linewidth=0.5, color='#F4F6F7', edgecolor='gray')
+        self.gdf.plot(ax=ax, linewidth=0.5, **kwargs)
 
-        cmap = get_cmap('crest')
+        cmap = get_cmap(points_palette)
         scatterplot(y=lat_col, x=lon_col, hue='ocupado', data=self.df, ax=ax, palette=cmap)
 
-        self.set_custom_legend(ax, cmap, values=[(0.0, 'Vacío'), (0.5, 'Ocupado'), (1.0, 'Lleno')])
+        self.set_custom_legend(ax, cmap, values=[(0.0, 'Hay bicis'), (0.5, 'Puede haber'), (1.0, 'No hay bicis')])
         fig.savefig(self.base_dir.joinpath('media','map','map.png'))
         display(fig)
 
 
-    def get_map(self) -> None:
+    def get_map(self, **kwargs) -> None:
         self.get_token(first_time=True)
         self.st = self.get_data()
         self.av = self.get_data(availability=True)
         self.get_shapefile()
         self.transform()
-        self.plot_map()
+        self.plot_map(**kwargs)

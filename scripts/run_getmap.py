2fa5711ef5cd61d0873e9e1b2858bc9107143dd2
EF <efraisma.ef7@gmail.com>
2022-04-14 15:46:37 -0500

    media folder moved to scripts directory

--- scripts/run_getmap.py
+++ scripts/run_getmap.py
@@ -3,6 +3,8 @@ from os import environ
 
 ECOBICI_CLIENT_ID = environ['ECOBICI_CLIENT_ID']
 ECOBICI_CLIENT_SECRET = environ['ECOBICI_CLIENT_SECRET']
+# ECOBICI_CLIENT_ID = '2199_132ley5lk3404wkk4c4w4ggo48kwcokosogg0k0www84s08gs'
+# ECOBICI_CLIENT_SECRET = '61xytd2ketssok44g4kkckwogkg048gk0ok48sc0k0wgc8scs'
 
 if __name__ == "__main__":
     ebm = EcoBiciMap(ECOBICI_CLIENT_ID, ECOBICI_CLIENT_SECRET)

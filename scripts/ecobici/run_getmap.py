81bd801229b79dd25998e1380f575d8a1f83ad76
EF <efraisma.ef7@gmail.com>
2022-04-14 13:09:48 -0500

    Secret keys

--- scripts/ecobici/run_getmap.py
+++ scripts/ecobici/run_getmap.py
@@ -1,8 +1,8 @@
 from map import EcoBiciMap
 from os import environ
 
-ECOBICI_CLIENT_ID = '2199_132ley5lk3404wkk4c4w4ggo48kwcokosogg0k0www84s08gs' #environ['ECOBICI_CLIENT_ID']
-ECOBICI_CLIENT_SECRET = '61xytd2ketssok44g4kkckwogkg048gk0ok48sc0k0wgc8scs' #environ['ECOBICI_CLIENT_SECRET']
+ECOBICI_CLIENT_ID = environ['ECOBICI_CLIENT_ID']
+ECOBICI_CLIENT_SECRET = environ['ECOBICI_CLIENT_SECRET']
 
 if __name__ == "__main__":
     ebm = EcoBiciMap(ECOBICI_CLIENT_ID, ECOBICI_CLIENT_SECRET)

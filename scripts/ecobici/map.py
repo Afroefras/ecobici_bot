dfffddef13d971d2de448c95c383f2c0795e41e7
EF <efraisma.ef7@gmail.com>
2022-04-14 13:07:33 -0500

    Get csv data every half hour

--- scripts/ecobici/map.py
+++ scripts/ecobici/map.py
@@ -117,6 +117,6 @@ class EcoBiciMap:
         self.av = self.get_data(availability=True)
         self.get_shapefile()
         self.transform()
-        now = datetime.now().strftime("%m-%d-%YT%H:%M")
+        now = datetime.now().strftime(r"%Y-%m-%dT%H:%M")
         self.df.to_csv(self.base_dir.joinpath('data', 'csv', f'data_{now}.csv'), index=False)
         self.plot_map(**kwargs)

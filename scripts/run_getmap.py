9cbffc550e893eb1958541a7fca418f599580ade
DiegoFores <41347420+DiegoFores@users.noreply.github.com>
2022-06-03 10:19:07 -0500

    Update run_getmap.py

--- scripts/run_getmap.py
+++ scripts/run_getmap.py
@@ -13,12 +13,12 @@ TWITTER_ACCESS_TOKEN_SECRET = environ['TWITTER_ACCESS_TOKEN_SECRET']
 
 if __name__ == "__main__":
     ebm = EcoBiciMap(ECOBICI_CLIENT_ID, ECOBICI_CLIENT_SECRET, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET, is_local=False)
-    # ebm.get_map(
-    #     img_name='future_map',
-    #     shp_first_time=False, 
-    #     padding=0.006,
-    #     # color='#1B1B1B',
-    #     color='#ffffff',
-    #     edgecolor='#00acee', 
-    #     points_palette='Blues'
-    # )
+    ebm.get_map(
+        img_name='future_map',
+        shp_first_time=False, 
+        padding=0.006,
+        # color='#1B1B1B',
+        color='#ffffff',
+        edgecolor='#00acee', 
+        points_palette='Blues'
+    )
